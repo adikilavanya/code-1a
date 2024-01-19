a7d5fcd2ffc8f277d0d593670d07a4b68158b729
lavanya <adikilavanya.2001@gmail.com>
2024-01-19 15:32:37 +0000

    sloution

---
+++ src/components/MusicPlayList/styledComponents.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+`
+export const ArtistBg = styled.div`
+  display: flex;
+`
+export const ArtistName = styled.h1`
+  color: black;
+`
+export const ArtistInfo = styled.p`
+  color: black;
+`
+export const PlayListContainer = styled.div`
+  display: flex;
+`
+export const PlayListHeading = styled.h1`
+  color: black;
+`
+export const SearchInput = styled.input`
+  color: black;
+`
+export const MusicList = styled.p`
+  color: black;
+`
+export const EmptyContainer = styled.div`
+  display: flex;
+`
+export const NoSongsText = styled.p`
+  color: black;
+`
